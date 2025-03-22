df2f1255653cbd89eac701a0673cce1b261bc8a1
Wonwoo Choi <chwo9843@gmail.com>
2025-03-23 00:50:16 +0900

    systems(chihiro): Add user papermc

--- systems/chihiro/users.nix
+++ systems/chihiro/users.nix
@@ -18,5 +18,26 @@
       packages = with pkgs; [
       ];
     };
+
+    papermc =
+      let
+        papermc = pkgs.papermc.override {
+          version = "1.21.4-214";
+          hash = "";
+        };
+      in
+      {
+        isSystemUser = true;
+        description = "PaperMC";
+        group = "papermc";
+        home = "/srv/minecraft";
+        packages = [
+          papermc
+        ];
+      };
+  };
+
+  users.groups = {
+    papermc = {};
   };
 }
