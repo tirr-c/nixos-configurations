11b4c63ffa4cf9803feac16d9cfa8962a7e6373a
Wonwoo Choi <chwo9843@gmail.com>
2025-05-03 00:43:28 +0900

    systems/profiles/nvidia: Use latest beta driver

--- systems/profiles/nvidia.nix
+++ systems/profiles/nvidia.nix
@@ -1,9 +1,21 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
+let
+  driverVersion = {
+    version = "575.51.02";
+    sha256_64bit = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
+    sha256_aarch64 = "sha256-NNeQU9sPfH1sq3d5RUq1MWT6+7mTo1SpVfzabYSVMVI=";
+    openSha256 = "sha256-NQg+QDm9Gt+5bapbUO96UFsPnz1hG1dtEwT/g/vKHkw=";
+    settingsSha256 = "sha256-6n9mVkEL39wJj5FB1HBml7TTJhNAhS/j5hqpNGFQE4w=";
+    persistencedSha256 = "sha256-dgmco+clEIY8bedxHC4wp+fH5JavTzyI1BI8BxoeJJI=";
+  };
+in
 {
   # NVIDIA driver
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver driverVersion;
+
     modesetting.enable = true;
     powerManagement = {
       enable = true;
