03713068d46157c57a1c7fec4b279b6bf40122c9
Wonwoo Choi <chwo9843@gmail.com>
2025-07-29 23:59:22 +0900

    systems/herta: Update disks

--- systems/herta/disks.nix
+++ systems/herta/disks.nix
@@ -2,25 +2,25 @@
 
 {
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0c0bac2c-6e17-495d-a2d6-680f4c1cc2ba";
+    device = "/dev/disk/by-uuid/d1a65dde-72f6-4f88-b800-bfdf1171f2fe";
     fsType = "btrfs";
     options = ["subvol=@"];
   };
 
-  boot.initrd.luks.devices."luks-d462a9cf-85db-49ae-b94e-ab9e2b19a915" = {
-    device = "/dev/disk/by-uuid/d462a9cf-85db-49ae-b94e-ab9e2b19a915";
+  boot.initrd.luks.devices."luks-b2ab300b-5e98-4eca-b4c0-acfd5a7ad033" = {
+    device = "/dev/disk/by-uuid/b2ab300b-5e98-4eca-b4c0-acfd5a7ad033";
     allowDiscards = true;
     bypassWorkqueues = true;
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/A052-8DAF";
+    device = "/dev/disk/by-uuid/B26D-DC6A";
     fsType = "vfat";
     options = ["fmask=0077" "dmask=0077"];
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/4a6925b1-2f3b-4640-9253-e21ff06416c1"; }
+    { device = "/dev/disk/by-uuid/ad423bf9-bec1-4c9d-ab94-5b67ace276df"; }
   ];
 
   services.btrfs.autoScrub.enable = true;
