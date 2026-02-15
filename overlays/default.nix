4788c8b8ad82f2631862daafbcbd06f90021830e
Wonwoo Choi <chwo9843@gmail.com>
2026-02-15 12:54:42 +0900

    systems/profiles/gaming: Remove unused overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,10 +6,6 @@
     p7zip = prev.p7zip.override { enableUnfree = true; };
   });
 
-  mbedtls = (final: prev: {
-    mbedtls_2 = final.mbedtls;
-  });
-
   libdispatch-disable-swift = (final: prev: {
     swift-corelibs-libdispatch = prev.swift-corelibs-libdispatch.override { useSwift = false; };
   });
--- systems/profiles/gaming.nix
+++ systems/profiles/gaming.nix
@@ -5,10 +5,6 @@
     inputs.aagl.nixosModules.default
   ];
 
-  nixpkgs.overlays = with inputs.self.overlays; [
-    mbedtls
-  ];
-
   nix.settings = {
     extra-substituters = lib.mkAfter [
       "https://ezkea.cachix.org"
