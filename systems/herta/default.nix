16be8c3101bda02ec1df52786b692591633cc5d2
Wonwoo Choi <chwo9843@gmail.com>
2026-03-08 23:56:30 +0900

    systems/herta: Allow building aarch64-linux with emulation

--- systems/herta/default.nix
+++ systems/herta/default.nix
@@ -191,6 +191,8 @@
     cpuFreqGovernor = "schedutil";
   };
 
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
