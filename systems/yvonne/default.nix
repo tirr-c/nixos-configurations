e607c2b77ac4e507f4011d149a14321ab6b43421
Wonwoo Choi <chwo9843@gmail.com>
2026-03-15 23:26:08 +0900

    systems/yvonne: Add serial console

--- systems/yvonne/default.nix
+++ systems/yvonne/default.nix
@@ -14,6 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
+  boot.kernelParams = ["console=ttyS0,115200n8"];
 
   boot.kernelPackages = pkgs.linuxPackages_6_18;
 
