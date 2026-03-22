9ac210feb56f9cdd848520e9d82ca02783dd32e5
Wonwoo Choi <chwo9843@gmail.com>
2026-03-22 16:05:29 +0900

    systems/chihiro: Update nocodb-webhooks-deps hash

--- systems/chihiro/nocodb-webhooks/app.nix
+++ systems/chihiro/nocodb-webhooks/app.nix
@@ -17,7 +17,7 @@ let
     hash = "sha256-9CwmJ40A9ZB+GsEjt4NvOoutZ4+uBPxIf7UhnwBsfGg=";
   };
 
-  depsHash = "sha256-aEr//97fG/2brG5cacXYKBnVNzGzQ0d/0wC18qv8CdM=";
+  depsHash = "sha256-oxKfRREAPF4IfoR6dPFfLvuGv7F73pQ9pYnU6k1JmdI=";
   denoDeps = stdenvNoCC.mkDerivation {
     name = "${pname}-deps";
     inherit src;
