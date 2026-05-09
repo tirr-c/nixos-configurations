4e41cbbf57d79970140a28e78c3e864dd31df8c6
Wonwoo Choi <chwo9843@gmail.com>
2026-05-09 19:26:29 +0900

    Update flake.lock

--- systems/chihiro/nocodb-webhooks/app.nix
+++ systems/chihiro/nocodb-webhooks/app.nix
@@ -17,7 +17,7 @@ let
     hash = "sha256-9CwmJ40A9ZB+GsEjt4NvOoutZ4+uBPxIf7UhnwBsfGg=";
   };
 
-  depsHash = "sha256-hYPTG8rRUhzViaQ/DMoO8ZhUpYXn2xs634mUQckzMxw=";
+  depsHash = "sha256-7Kr4dQBweGnV4//PJk4Gcec5kzfx59EShDIQq8H6j6M=";
   denoDeps = stdenvNoCC.mkDerivation {
     name = "${pname}-deps";
     inherit src;
