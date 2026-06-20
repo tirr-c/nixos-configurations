3d7022c33dd904b50425a0d82bbb3b4201c32261
Wonwoo Choi <chwo9843@gmail.com>
2026-06-20 17:07:29 +0900

    systems/chihiro: Allow unattended boot

--- systems/chihiro/disks.nix
+++ systems/chihiro/disks.nix
@@ -14,26 +14,32 @@
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/c7668a70-fb50-4415-afd9-0476188cd2c7";
     fsType = "btrfs";
-    options = [
-      "noatime"
-      "subvol=@"
-    ];
+    options = ["noatime" "subvol=@"];
   };
 
-  #boot.initrd.supportedFilesystems = ["vfat"];
   boot.initrd.luks.devices."luks-a20b886e-ee6b-4cdf-91b4-5630b58c7c58" = {
     device = "/dev/disk/by-uuid/a20b886e-ee6b-4cdf-91b4-5630b58c7c58";
-    #keyFile = "/tmp/keys/key-0:UUID=4FC5-A53F";
+    keyFile = "/mnt/keys/key-0";
+    keyFileTimeout = 5;
+    allowDiscards = true;
     bypassWorkqueues = true;
   };
+
+  boot.initrd.availableKernelModules = ["usb_storage" "xhci_pci" "ohci_pci" "ehci_pci"];
+  boot.initrd.supportedFilesystems = ["vfat"];
+  boot.initrd.systemd.mounts = [
+    {
+      what = "/dev/disk/by-uuid/4FC5-A53F";
+      where = "/mnt/keys";
+      type = "vfat";
+      options = "ro,fmask=0077,dmask=0077";
+    }
+  ];
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/3554-562C";
     fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
+    options = ["fmask=0077" "dmask=0077"];
   };
 
   swapDevices = [
