4dea81c6eb5ccd278f7050196ad46777f35f76cb
Wonwoo Choi <chwo9843@gmail.com>
2026-06-21 15:51:53 +0900

    systems/plachta: Add nix-store private key

--- systems/plachta/agenix.nix
+++ systems/plachta/agenix.nix
@@ -4,4 +4,11 @@
   imports = [
     ../../secrets
   ];
+
+  age.identityPaths = ["/etc/agenix-host-key"];
+  age.rekey.hostPubkey = ./host-pubkey.pub;
+
+  age.secrets = {
+    nix-store-private-key.rekeyFile = ../../secrets/master/plachta-nix-store-private-key.age;
+  };
 }
