8b2c80eea726aeaa3f4353f13b484e775770fed6
Wonwoo Choi <chwo9843@gmail.com>
2026-07-26 04:12:38 +0900

    systems/chihiro: Update immich settings

--- systems/chihiro/immich.nix
+++ systems/chihiro/immich.nix
@@ -12,6 +12,36 @@
     accelerationDevices = ["/dev/dri/renderD128"];
     settings = {
       server.externalDomain = "https://photos.veritas.tirr.network";
+
+      oauth = {
+        enabled = true;
+        autoLaunch = true;
+        autoRegister = true;
+        buttonText = "Login with Keycloak";
+
+        clientId = "immich";
+        issuerUrl = "https://keycloak.veritas.tirr.network/realms/master";
+        signingAlgorithm = "ES256";
+        scope = "openid email profile";
+        roleClaim = "immich_role";
+        storageLabelClaim = "preferred_username";
+        storageQuotaClaim = "immich_quota";
+        timeout = 30000;
+
+        mobileOverrideEnabled = false;
+
+        defaultStorageQuota = null;
+      };
+
+      ffmpeg = {
+        accel = "nvenc";
+      };
+
+      # Tuned for Korean
+      machineLearning = {
+        clip.modelName = "nllb-clip-base-siglip__mrl";
+        ocr.modelName = "KOREAN__PP-OCRv5_mobile";
+      };
     };
   };
 }
