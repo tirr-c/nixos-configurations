79eeabbcc5186fe1e458b2272b3f5dfe5fea74db
Wonwoo Choi <chwo9843@gmail.com>
2026-08-02 02:12:16 +0900

    systems/sophie: Setup trusted public key for build machine

--- systems/sophie/default.nix
+++ systems/sophie/default.nix
@@ -8,6 +8,10 @@
     ./agenix.nix
   ];
 
+  nix.settings.trusted-public-keys = [
+    (builtins.readFile ../frieren/nix-store-public-key.pub)
+  ];
+
   time.timeZone = "Asia/Seoul";
 
   i18n.defaultLocale = "en_US.UTF-8";
