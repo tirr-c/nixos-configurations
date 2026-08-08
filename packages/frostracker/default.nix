09eb1abe865a9fe11172317ce3b148d2f2c886f1
Wonwoo Choi <chwo9843@gmail.com>
2026-08-08 14:52:29 +0900

    packages/frostracker: Update

--- packages/frostracker/default.nix
+++ packages/frostracker/default.nix
@@ -8,14 +8,14 @@
 
 let
   pname = "frostracker";
-  rev = "a94ed58ed93f3a506f7d02ce70fd4f02c1f855e2";
+  rev = "65c1d713feb84e13fb63eff1b7f3138b0b34c444";
   npmDepsHash = "sha256-xLUPn4JPM2MC9N6tY8RvsB9AFTBo12nuK/QsWlU1Xfo=";
 
   app = buildNpmPackage {
     inherit pname;
     version = "0-unstable-${rev}";
 
-    src = builtins.fetchGit {
+    src = fetchGit {
       url = "ssh+git://git@github.com/ektnzpt1080/frosthaven-tracker.git";
       inherit rev;
     };
