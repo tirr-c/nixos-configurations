e918c53763d24b646e29c14c0ae21c86c275f13f
Wonwoo Choi <chwo9843@gmail.com>
2026-09-06 16:32:43 +0900

    users/tirr: Remove store helper

--- users/tirr/minimal.nix
+++ users/tirr/minimal.nix
@@ -60,8 +60,6 @@ in
   programs.git-credential-oauth.enable = true;
   programs.git.settings.credential = {
     "https://git.tirr.dev" = {
-      helper = lib.mkBefore ["store"];
-
       oauthClientId = "4cd1e89a-39fe-42d5-a5de-e0bef6865582";
       oauthAuthURL = "/login/oauth/authorize";
       oauthTokenURL = "/login/oauth/access_token";
